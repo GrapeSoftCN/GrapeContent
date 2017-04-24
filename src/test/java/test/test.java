38380573ba25fed6f92e156047583e20e88f5363
GrapeSoftCN <jk282@163.com>
2017-04-24 19:36:47 +0800

    修改接口，增加了新增操作的权限判断

--- src/test/java/test/test.java
+++ src/test/java/test/test.java
@@ -3,7 +3,15 @@ package test;
 import interfaceApplication.Content;
 
 public class test {
+//	private Content content = new Content();
 	public static void main(String[] args) {
-		System.out.println(new Content().ShowByGroupId("58f62d741a4769cbf53907b6"));
+		Content content = new Content();
+		long start = System.currentTimeMillis();
+		for (int i = 0; i < 500; i++) {
+			System.out.println(content.ShowByGroupId("58f62d741a4769cbf53907b6"));
+		}
+//		System.out.println(content.ShowByGroupId("58f62d741a4769cbf53907b6"));
+		long end = System.currentTimeMillis();
+		System.out.println(end-start);
 	}
 }
